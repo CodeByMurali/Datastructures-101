8cfaa818961adfae8460aabded49d468b480bc8a
murali.rajendran <murali.rajendran@veeva.com>
2024-05-08 22:07:03 -0400

    LinkedList Practice

--- src/DataStructures/LinkedList/LinkedListTwo.java
+++ src/DataStructures/LinkedList/LinkedListTwo.java
@@ -1,91 +1,123 @@
 package DataStructures.LinkedList;
 
+
+import org.w3c.dom.Node;
+
 import java.util.NoSuchElementException;
 
 public class LinkedListTwo {
-    private class Node {
-        public int value;
-        public Node next;
 
-        public Node(int value) {
+    private class Node{
+        private int value;
+        private Node next;
+
+        public void setValue(int value){
             this.value = value;
         }
-
     }
 
+    int count;
     private Node first;
     private Node last;
-    private int size;
+    private Node previous;
 
-    public void addLast(int item){
-        var node = new Node(item);
-        if(isEmpty()){
+
+    // addLast
+    public void addLast(int value) {
+        // Create a new node
+        var node = createNode(value);
+
+        // Verifies that there were no nodes created so far
+        if (first == null) {
+            // Point the head and the tail to the node that is created
+            // As there is only one node
             first = last = node;
-        }
-        else {
+        } else {
+            // Point the previous last node's next pointer to the node that is just created
             last.next = node;
+            // Point the tail to the last node that is just added
             last = node;
         }
-        size ++;
+        count ++;
     }
-    public void addFirst(int item){
-        var node = new Node(item);
-        if (isEmpty()){
+
+    public void addFirst(int value) {
+        // Create a new node
+        var node = createNode(value);
+
+        // Verifies that there were no nodes created so far
+        if (first == null) {
+            // Point the head and the tail to the node that is created
+            // As there is only one node
             first = last = node;
-        }
-        else {
+        } else {
             node.next = first;
             first = node;
         }
-        size++;
+        count ++;
     }
-    public int indexOf(int item){
-        if(isEmpty()){
-            throw new NoSuchElementException();
-        }
-        var currentNode = first;
-        int index = 0;
-        while (currentNode != null){
-            if(currentNode.value == item){
-                return index;
+
+    // Private method to create a node and set its value
+    private Node createNode(int value) {
+        // Create a new node
+        var node = new Node();
+        // Set the value to the node
+        node.setValue(value);
+        return node;
+    }
+
+    // indexOf
+    public int indexOf(int value){
+        //Element not found
+        int index = -1;
+        int iterator = 0;
+        while (first.next != null){
+            if(first.value == value){
+                index = iterator;
             }
-            currentNode = currentNode.next;
-            index++;
+            first = first.next;
+            iterator ++;
         }
-        return -1;
+        return index;
     }
-    public void removeFirst(){
-        if(isEmpty()){
+
+    // deleteFirst
+    public void removeFirst() {
+        if (first == null) {
             throw new NoSuchElementException();
         }
-        else{
-            var secondNode = first.next;
+        // if there is only one node, since it also acts as the first that must be removed
+        // Hence remove that node by setting it to null and return statement would end the execution
+        if (first == last) {
+            first = last = null;
+        } else {
+            Node secondNode = first.next;
+            //Removing the link between first and the second node
             first.next = null;
+
+            //Pointing the first to second node
             first = secondNode;
         }
+        count --;
     }
+
+    // deleteLast
     public void removeLast(){
-        if(isEmpty()){
+        if (last == null) {
             throw new NoSuchElementException();
         }
+        // if there is only one node, since it also acts as the last that must be removed
+        // Hence remove that node by setting it to null and return statement would end the execution
+        if (first == last){
+            first = last = null;
+        }
         else{
-            last = getpreviousNode(last);
+            var previous = getpreviousNode(last);
+            last = previous;
             last.next = null;
-        }
 
-        size --;
-    }
-    public int[] toArray(){
-        int[] array = new int[size];
-        var currentNode = first;
-        int index = 0;
-
-        while (currentNode !=  null){
-            array[index] = currentNode.value;
-            currentNode = currentNode.next;
-            index++;
         }
-        return array;
+        count --;
     }
 
     private Node getpreviousNode(Node node){
@@ -97,10 +129,71 @@ public class LinkedListTwo {
         }
         return null;
     }
-    private boolean isEmpty(){
-        return first == null;
+
+    // contains
+    public boolean contains(int value){
+        return indexOf(value) != -1;
     }
-    public boolean contains(int item){
-        return false;
+
+    //Size
+    public int size(){
+        return count;
     }
+
+    //Convert to Array
+    public int[] toArray(){
+        int[] array = new int[count];
+        int index = 0;
+        Node currentNode = first;
+        while (currentNode != null){
+            array[index++] = currentNode.value;
+            currentNode = currentNode.next;
+        }
+        return array;
+    }
+
+//    public void reverse(){
+//
+//        // 10 -> 20 -> 30
+//        Node previous = first; //10
+//        Node current = first.next; //20
+//
+//
+//        while (current != null){
+//            Node next = current.next; //30
+//
+//            //20 -> 30
+//            current.next = previous;
+//            previous = current; //20
+//            current = next; //30
+//        }
+////        var prevlast = last;
+////        last = first;
+////        first = prevlast;
+//
+//        last = first;
+//        last.next = null;
+//        first = previous;
+//    }
+
+    public void reverse(){
+
+        var current = first.next;
+        var previous = first;
+
+
+
+        while (current != null){
+            var next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+
+        last = first;
+        last.next = null;
+        first = previous;
+    }
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
 import DataStructures.Arrays.ArraysPractice;
+import DataStructures.LinkedList.LinkedListTwo;
+
+import java.util.Arrays;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -12,22 +15,21 @@ public class Main {
 //       fizzBuzzCalc();
 
         //Arrays
-        ArraysPractice numbers = new ArraysPractice(3);
-        numbers.insert(20);
-        numbers.insert(30);
-        numbers.insert(40);
-        numbers.insert(50);
-        numbers.insert(60);
-        numbers.insert(70);
-        numbers.insert(80);
-        numbers.insertAt(0, 0);
-        numbers.reverse();
-        System.out.println(numbers.max());
+//        ArraysPractice numbers = new ArraysPractice(3);
+//        numbers.insert(10);
+//        numbers.insert(20);
+//        numbers.insert(30);
+//        numbers.insert(50);
+//        numbers.insert(60);
+//        numbers.insert(70);
+//        numbers.insert(80);
+//        numbers.insertAt(0, 0);
+//        numbers.reverse();
+//        System.out.println(numbers.max());
 //        numbers.removeAt(2);
 //        numbers.removeAt(3);
 //        System.out.println(numbers.indexOf(50));
 //        numbers.print();
-
 //        numbers.removeAt(1);
 //        System.out.println(numbers.indexOf(50));
 //        System.out.println(numbers.max());
@@ -37,14 +39,21 @@ public class Main {
 //        numbers.print();
 
         //LinkedList
-//        LinkedListTwo list = new LinkedListTwo();
-//        list.addLast(1);
-//        list.addLast(2);
-//        list.addLast(3);
+        LinkedListTwo list = new LinkedListTwo();
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+        list.reverse();
 //        list.addLast(4);
+//        list.addLast(5);
+//        list.addLast(6);
 //        list.addFirst(0);
-//        System.out.println(list.indexOf(3));
+//        System.out.println(list.indexOf(0));
 //        list.removeFirst();
+//        System.out.println(list.size());
+
+        var array = list.toArray();
+        System.out.println(Arrays.toString(array));
 //        list.removeLast();
 //        System.out.println(list.indexOf(20));
 //        System.out.println(list.contains(20));
